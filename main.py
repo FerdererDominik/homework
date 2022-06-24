a13eee4bf9db836901992ab04a3947d84a8ec423
FerdererDominik <107643212+FerdererDominik@users.noreply.github.com>
2022-06-24 05:53:16 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,98 @@
-# 1
-# dict_1 = {}
-# i = 0
-# while i < 5:
-#     i += 1
-#     num = int(input("Введите число"))
-#     dict_1[num] = num * num
-#     # print(dict_1)
-# print(dict_1)
+#1
+# a = int(input("Введите число:"))
+# b = int(input("Введите число:"))
+# c = int(input("Введите число:"))
+# if a > b and a > c:
+#     print(f"Наибольшое число:{a}")
+# elif b > a and b > c:
+#     print(f"Наибольшое число:{b}")
+# elif c > a and c > b:
+#     print(f"Наибольшое число:{c}")
+# else:
+#     print(f"Одного наибольшего числа нет")
 
 # 2
-# sm = 0
-# dict_1 = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
-# for i, j in dict_1.items():
-#     sm = sm + dict_1[i]
-#     print(sm)
-# sr = sm / len(dict_1)
-# print(sr)
+# def sm (lst):
+#     s = 0
+#     for num in lst:
+#         s += num
+#     print(s)
+#     return s
+# list_1 = [8, 2, 3, 0, 7]
+# sm(list_1)
 
 # 3
-# list_1 = [1, 3, 5, 7, 9]
-# list_2 = [2, 4, 6, 8, 10]
-# print(dict(zip(list_1, list_2)))
+# def umn (lst):
+#     sm = 1
+#     for num in lst:
+#         sm *= num
+#     print(sm)
+#     return sm
+# list_1 = [8, 2, 3, -1, 7]
+# umn(list_1)
 
 # 4
-# dict_1 = {"Moscow": (550, 370), "London": (510, 510), "Paris": (480, 480)}
-# dist = {}
-# ML = ((550 - 370) ** 2 + (510 - 510) ** 2) ** 0,5
-# dist["Msc => London"] = ML
-# MP = ((550 - 370) ** 2 + (480 - 480) ** 2) ** 0,5
-# dist["Msc => Paris"] = MP
-# LP = ((510 - 510) ** 2 + (480 - 480) ** 2) ** 0,5
-# dist["London => Paris"] = LP
-# print(dist)
-# Я слегка не понял как выполнять это задание
+# a = "1234abcd"
+# print(a[::-1])
 
 # 5
-# goods = {"Лампа": '12345', "Стол": '23456', "Диван": '34567', "Стул": '45678'}
-# store = {'12345': [{'quanity': 27, 'price': 42}],
-#          '23456': [{'quanity': 22, 'price': 510}, {'quanity': 31, 'price': 520}],
-#          '34567': [{'quanity': 2, 'price': 1200}, {'quanity': 1, 'price': 1150}],
-#          '45678': [{'quanity': 50, 'price': 100}, {'quanity': 12, 'price': 95}, {'quanity': 43, 'price': 97}]}
-# print(store[goods["Лампа"]])
-# while True:
-#     command = int(input(
-#         'Введите команду:\n\t1-"Лампа"\n\t2-"Стол"'
-#         '\n\t3-"Диван"\n\t4-"Стул"\nВаша команда'))
-#     if command == 1:
-#         # for i, j in store[goods["Лампа"]]:
-#         print(f"Стоимость всех стульев:{27 * 42}")
-#     elif command == 2:
-#         print(f"Стоимость всех столов:{(22 * 510) + (31 * 520)}")
-#     elif command == 3:
-#         print(f"Стоимость всех диванов:{(2 * 1200) + (1 * 1150)}")
-#     elif command == 4:
-#         print(f"Стоимость всех стульев:{(50 * 100) + (12 * 95) + (42 * 97)}")
+# def fact (num):
+#     i = 0
+#     f = 1
+#     while i < num:
+#         i += 1
+#         f *= i
+#     print(f)
+#     return f
+# a = int(input("Введите число для факториала:"))
+# fact(a)
+
+# 6
+# def str(z):
+#     up = 0
+#     low = 0
+#     for i in z:
+#         if i.isupper():
+#             up += 1
+#         elif i.islower():
+#             low += 1
+#         else:
+#             continue
+#     print(f"Кол-во символов верхнего регистра:{up}")
+#     print(f"Кол-во символов нижнеего регистра:{low}")
+#
+# a = input()
+# str(a)
+
+
+# 7
+# def plmdr(str):
+#     ltr = str[::-1]
+#     if str == ltr:
+#         print("Это слово является палиндромом")
 #     else:
-#         print("Такого номера нет")
+#         print("Это слово не является палиндромом")
+# word = input()
+# plmdr(word)
+
+#
+# 8
+# def bank(year, money):
+#     i = 0
+#     while i < year:
+#         i += 1
+#         money += money / 10
+#     print(money)
+#     return money
+# y = int(input("Введите кол-во лет:"))
+# m = int(input("Введите кол-во денег:"))
+# bank(y, m)
+#
+# # 9
+# def dl(lst):
+#     for num in lst:
+#         if num % 15 == 0:
+#             print(num)
+#     return num
+# list_1 = [45, 55, 60, 37, 105, 220]
+# dl(list_1)
