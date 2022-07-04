0b55475482132393c910a1ed6111f03ed37cf338
FerdererDominik <107643212+FerdererDominik@users.noreply.github.com>
2022-07-05 04:18:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 # 1
-# def sum_of_numbers(n):
-#     a = n // 100
-#     b = n % 100 // 10
-#     c = n % 10
-#     return (a + b + c)
-# num = int(input("Введите трехзначное число"))
-# print(sum_of_numbers(num))
+# i = 0
+# with open("mbox-short.txt", "r", encoding="utf-8") as file:
+#     text = file.read()
+#     list_split = list(text.split())
+#     for mail in list_split:
+#         if '@' in mail and 'source' not in mail and '>' not in mail and ')' not in mail:
+#             print(mail)
+#             i += 1
+#     print(f'кол-во мейлов:{i}')
 
 # 2
-# def schet(secs):
-#     minuts = 0
-#     hours = 0
-#     days = 0
-#     for i in range(secs):
-#         if secs > 60:
-#             secs -= 60
-#             minuts += 1
-#         if minuts > 60:
-#             minuts -= 60
-#             hours += 1
-#         if hours > 24:
-#             hours -= 24
-#             days += 1
-#     return (f"дней: {days}\nчасов: {hours}\nминут: {minuts}\nсекунд:{secs}")
-# secunds = int(input("Введите кол-во секунд"))
-# print(schet(secunds))
+# list_1 = []
+# with open("romeo.txt", "r", encoding="utf-8") as file:
+#     text = list(file.read().split())
+#     sort = sorted(text)
+#     for str in sort:
+#         if str in list_1:
+#             continue
+#         list_1.append(str)
+#     print(list_1)
 
-# codewars
-# 1
+# 3
+# def read_last(lines, txt):
+#     with open(txt, "r", encoding="utf-8") as file:
+#         num = 0 - lines
+#         text = file.read()
+#         list_txt = list(text.split('\n'))
+        # for line in list_txt[num::]: я пробовал сделать через фор для красоты вывода, но в конце выдает none
+        #     print(line)
+#         return list_txt[num::]
+# text = input("Введите название файла:")
+# line = int(input("Введите кол-во выведенных последних строк"))
+# print(read_last(line, text))
+
+# 4
+# def long(txt):
+#     with open(txt, "r", encoding="utf-8") as file:
+#         text = list(file.read().split())
+#         list_longest = []
+#         longest = 0
+#         for word in text:
+#             if len(word) > longest:
+#                 longest = len(word)
+#         for lens in text:
+#             if longest == len(lens):
+#                 list_longest.append(lens)
+#         return list_longest, longest
+# file_name = input('Введите название файла:')
+# print(long(file_name))
